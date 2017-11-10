505949de9a21815f32c31188adf70766e348a962
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-10 15:34:10 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,5 +1,5 @@
 def division(num1, num2)
-  return num1/num2 
+  return num1/num2
 end
 
 def assign_variable(value)
@@ -10,18 +10,18 @@ def argue(x)
   return x
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(type="cheese")
+  return type
 end
